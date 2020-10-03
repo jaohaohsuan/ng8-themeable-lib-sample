7eb702afb529009c3c639f73ccedc92812b945d6
Henry Jao <henryjaonz@gmail.com>
2020-10-04 03:33:22 +1300

    test(prettier): test3

--- projects/web-app/src/app/app.component.ts
+++ projects/web-app/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'web-app';
+  title =                                                                  'web-app';
 }
